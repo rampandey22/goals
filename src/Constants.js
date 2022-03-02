2ac5edb720b006eff08e7c5a7fa668fbc7852fa1
Ram Pandey <ram.pandey22@outlook.com>
2022-03-02 22:37:00 +1100

    updated to call todoapp api

---
+++ src/Constants.js
@@ -0,0 +1,2 @@
+export const API_URL = 'http://a931637afe9d04ef7a6a7b4355689762-563553372.us-east-2.elb.amazonaws.com'
+export const JPA_API_URL = 'http://a931637afe9d04ef7a6a7b4355689762-563553372.us-east-2.elb.amazonaws.com/jpa'
