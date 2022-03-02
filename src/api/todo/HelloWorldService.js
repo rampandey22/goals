4d3ff8fadc2f7dee48655f3c342363741ba72e88
Ram Pandey <ram.pandey22@outlook.com>
2022-03-02 23:29:52 +1100

    changed the API url

--- src/api/todo/HelloWorldService.js
+++ src/api/todo/HelloWorldService.js
@@ -1,20 +1,20 @@
 import axios from 'axios'
-
+import { API_URL, JPA_API_URL } from '../../Constants'
 class HelloWorldService {
 
     executeHelloWorldService() {
         //console.log('executed service')
-        return axios.get('http://localhost:8080/hello-world');
+        return axios.get(`${JPA_API_URL}/hello-world`);
     }
 
     executeHelloWorldBeanService() {
         //console.log('executed service')
-        return axios.get('http://localhost:8080/hello-world-bean');
+        return axios.get(`${JPA_API_URL}/hello-world-bean`);
     }
 
     executeHelloWorldPathVariableService(name) {
        
-        return axios.get(`http://localhost:8080/hello-world/path-variable/${name}`
+        return axios.get(`${JPA_API_URL}/hello-world/path-variable/${name}`
            
         );
     }
